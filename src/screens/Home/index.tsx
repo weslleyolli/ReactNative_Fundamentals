374d5a5e501315af5522ec0c4ffc9c43ec91380e
weslleyolli <weslleyrichardson.cg@hotmail.com>
2023-11-02 00:10:20 -0300

    add button

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,16 +1,20 @@
 import { styles } from "./styles";
-import { Text, View, TextInput } from "react-native";
+import { Text, View, TextInput, TouchableOpacity } from "react-native";
 
 export function Home(){
   return(
     <View style={styles.container}>
-      <Text style={styles.eventName}>React Native</Text>
-      <Text style={styles.eventName}>Hello World</Text>
+      <Text style={styles.eventName}>Te amo </Text>
+      <Text style={styles.eventName}></Text>
       <TextInput 
         style={styles.input}
         placeholder="Participant name's"
         placeholderTextColor="#6B6B6B"
       />
+
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}> + </Text>
+      </TouchableOpacity>
     </View>
   )
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -16,10 +16,23 @@ export const styles = StyleSheet.create({
     },
     input:{
         height: 56,
+        width: "100%",
         backgroundColor: "#1F1E25",
         borderRadius: 5,
         color: "#FDFCFE",
         padding: 16,
         fontSize: 16,
+    },
+    buttonText:{
+        color: "#FFF",
+        fontSize: 24
+    },
+    button: {
+        width: 56,
+        height: 56,
+        borderRadius: 5,
+        backgroundColor: "#31CF67",
+        alignItems: "center",
+        justifyContent: "center",
     }
   })
